0a9355d273db7af1009c91aa987f0503edf0d932
ZEUS 3 <zeus14zeus1414@gmail.com>
2026-01-02 06:50:16 +0300

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     googleId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
+    role: { type: String, default: 'user', enum: ['user', 'admin'] } // إضافة الصلاحيات
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
